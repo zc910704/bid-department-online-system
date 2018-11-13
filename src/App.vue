2a7572a335803b155a4cdf1afa148c46ab0242ed
zc910704 <329188614@qq.com>
2018-11-13 20:18:28 +0800

    first commit

--- src/App.vue
+++ src/App.vue
@@ -1,23 +1,25 @@
 <template>
-  <ve-line :data="chartData"></ve-line>
+  <section class="body-warp">
+    <transition name="fade" mode="out-in">
+      <router-view></router-view>
+    </transition>
+  </section>
 </template>
-
-<script>
+<script type="text/javascript">
   export default {
-    data: function () {
-      return {
-        chartData: {
-          columns: ['日期', '访问用户', '下单用户', '下单率'],
-          rows: [
-            { '日期': '1/1', '访问用户': 1393, '下单用户': 1093, '下单率': 0.32 },
-            { '日期': '1/2', '访问用户': 3530, '下单用户': 3230, '下单率': 0.26 },
-            { '日期': '1/3', '访问用户': 2923, '下单用户': 2623, '下单率': 0.76 },
-            { '日期': '1/4', '访问用户': 1723, '下单用户': 1423, '下单率': 0.49 },
-            { '日期': '1/5', '访问用户': 3792, '下单用户': 3492, '下单率': 0.323 },
-            { '日期': '1/6', '访问用户': 4593, '下单用户': 4293, '下单率': 0.78 }
-          ]
-        }
-      }
-    }
+    name: 'app'
   }
 </script>
+<style lang="scss" type="text/scss" rel="stylesheet/scss">
+  @import '~assets/scss/main';
+
+  .fade-enter-active,
+  .fade-leave-active {
+    transition: all .2s ease;
+  }
+
+  .fade-enter,
+  .fade-leave-active {
+    opacity: 0;
+  }
+</style>
--- src/main.js
+++ src/main.js
@@ -3,8 +3,17 @@ import App from './App.vue'
 import VCharts from 'v-charts'
 Vue.use(VCharts)
 
+import ElementUI from 'element-ui';
+import 'element-ui/lib/theme-chalk/index.css';
+Vue.use(ElementUI);
+
+import VueRouter from 'vue-router'
+import router from './router'
+Vue.use(VueRouter)
+
 Vue.config.productionTip = false
 
 new Vue({
+  router,
   render: h => h(App),
 }).$mount('#app')
