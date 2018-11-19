289827c24c4ceb8bee412dd43673bf929dbb7632
zc910704 <329188614@qq.com>
2018-11-19 21:07:17 +0800

    del extra router

--- src/views/detail/index.vue
+++ src/views/detail/index.vue
@@ -1,29 +1,39 @@
 <template>
-  <el-table
-    :data="tableData"
-    border
-    style="width: 100%">
-    <el-table-column
-      prop="date"
-      label="日期"
-      width="180"/>
-    <el-table-column
-      prop="company"
-      label="公司"
-      width="180"/>
-    <el-table-column
-      prop="item"
-      label="投标项目"/>
-    <el-table-column
-      prop="price"
-      label="报价"/>
-    <el-table-column
-      prop="rate"
-      label="下浮率"/>
-    <el-table-column
-      prop="isGot"
-      label="是否中标"/>
-  </el-table>
+  <div>
+    <el-form :inline="true" :model="formInline" class="demo-form-inline" style="margin-top: 20px;margin-left:10px">
+      <el-form-item label="搜索">
+        <el-input v-model="formInline.search" placeholder="输入搜索内容"/>
+      </el-form-item>
+      <el-form-item>
+        <el-button type="primary" @click="onSubmit">查询</el-button>
+      </el-form-item>
+    </el-form>
+    <el-table
+      :data="tableData"
+      border
+      style="width: 100%">
+      <el-table-column
+        prop="date"
+        label="日期"
+        width="180"/>
+      <el-table-column
+        prop="company"
+        label="公司"
+        width="180"/>
+      <el-table-column
+        prop="item"
+        label="投标项目"/>
+      <el-table-column
+        prop="price"
+        label="报价"/>
+      <el-table-column
+        prop="rate"
+        label="下浮率"/>
+      <el-table-column
+        prop="isGot"
+        label="是否中标"/>
+    </el-table>
+  </div>
 </template>
 
 <script>
@@ -47,11 +57,19 @@ export default {
           rate: '56%',
           isGot: this.isGottoString(false)
         }
-      ]
+      ],
+      formInline: {
+        search: ''
+      }
+    }
+  },
+  watch: {
+    formInline: function() {
+      console.log(this.formInline.search)
     }
   },
   methods: {
-    isGottoString: (val) => {
+    isGottoString: val => {
       if (val) {
         return '是'
       } else {
